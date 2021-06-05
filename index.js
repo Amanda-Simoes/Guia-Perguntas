75c169993b39da4faf93ebb7b2ae03a585700022
Amanda <amanda.simoes1707@gmail.com>
2021-06-05 15:46:18 -0300

    Renderizando index.ejs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine','ejs') // Configurando o express para utilizar o EJS como view engine
+
 app.get("/",(req,res) => {
-    res.send("Bem vindo ao meu site!")
+    res.render("index") // Renderizando a pagina index.ejs
 }) // Rota principal da aplicação
 
 app.listen(4000,() => {
